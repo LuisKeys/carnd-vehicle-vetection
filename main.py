6ad636e1e6858c89efd684f3d5313ecdbd416990
Luis <luis.paradela@gmail.com>
2017-08-03 14:54:50 -0300

    Heat map and lables

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import cv2
 import detection_library as dl
 import matplotlib.pyplot as plt
 import training_classifier as tc
+from sklearn.externals import joblib
+from moviepy.editor import VideoFileClip
 
 this = sys.modules[__name__]
 
@@ -10,6 +12,30 @@ this = sys.modules[__name__]
 def traing_classifier():
 	print('Train classifier')
 	this.X_scaler, this.svc  = tc.train_classifier()
+
+# Video Process callback function
+def process_image(image):
+
+	scale_factor = 255.0
+	# classify
+	out_image = dl.detect(image, this.svc, this.X_scaler, scale_factor,
+						  color_from='RGB', color_to='YCrCb')
+	return out_image
+
+# Video Process
+def test_video():
+	# Load trained svc and scaler
+	this.svc = joblib.load('svc_rbf_c10.pkl')
+	this.X_scaler = joblib.load('X_scaler.pkl')
+	# Videos paths:
+	video_input = './project_video.mp4'
+	video_output = './project_video_output.mp4'
+	clip = VideoFileClip(video_input).subclip(8, 16)
+	#clip = VideoFileClip(video_input)
+	white_clip = clip.fl_image(process_image)
+	# Save output video
+	white_clip.write_videofile(video_output, audio=False)
+ 
 
 # Test Image
 def test_image():
@@ -20,7 +46,8 @@ def test_image():
 	image = cv2.imread(path + file_name)	
 
 	scale_factor = 255.0
-
+	this.svc = joblib.load('svc_lin_c10.pkl')
+	this.X_scaler = joblib.load('X_scaler.pkl')
 	# classify
 	out_image = dl.detect(image, this.svc, this.X_scaler, scale_factor,
 						  color_from='BGR', color_to='YCrCb')
@@ -32,5 +59,6 @@ def test_image():
 	plt.show()
 
 
-traing_classifier()
+#traing_classifier()
 test_image()
+#test_video()
